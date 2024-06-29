9eae375108d49f9e4f48bdfd85d590189975fcc5
CipiVlad <supercoder.cipi@gmail.com>
2024-06-29 11:44:17 +0200

    media queries added hamburger icon main.js etc.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+const hamburger = document.getElementById('hamburger')
+let menu = document.getElementById('menu')
+
+// only show menu on mobile
+window.onload = () => {
+    if (window.innerWidth < 430) {
+        menu.style.display = 'none'
+    }
+}
+
+// toggle menu
+hamburger.addEventListener('click', () => {
+
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+
+})
